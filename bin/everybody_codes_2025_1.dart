2c6f304b4a417a9035695c09f8929e6bf2ad7e81
Ari Cummings <ari@aricummings.com>
2026-04-20 09:09:10 -0700

    Initial commit

---
+++ bin/everybody_codes_2025_1.dart
@@ -0,0 +1,5 @@
+import 'package:everybody_codes_2025_1/everybody_codes_2025_1.dart' as everybody_codes_2025_1;
+
+void main(List<String> arguments) {
+
+}
---
+++ test/everybody_codes_2025_1_test.dart
@@ -0,0 +1,3 @@
+import 'package:everybody_codes_2025_1/everybody_codes_2025_1.dart';
+import 'package:test/test.dart';
+
